a63d0243c5d7b99e8e9c1cab3c720bdf286823d3
whmsft <whmsft@outlook.com>
2024-09-29 20:48:22 +0530

    Updated stuff. Added images

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-#include <string> // to_string
-// #include <vector>
+#include <string>
 #include "include/raylib.h"
 #include "include/nlohmann/json.hpp"
 using namespace std;
@@ -16,6 +15,9 @@ nlohmann::json levelMap = nlohmann::json::parse(levelMapFile);
 int level = 1;
 int screenWidth, screenHeight, offsetX, offsetY, playerX, playerY;
 int levelWidth, levelHeight;
+
+Image imageCornerTopLeft, imageCornerTopRight, imageCornerBottomLeft, imageCornerBottomRight = LoadImage("assets/corner.png");
+Image imageWallUp, imageWallDown, imageWallLeft, imageWallRight = LoadImage("assets/wall.png");
 
 int main(void) {
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
@@ -41,18 +43,46 @@ void update() {
   playerX = levelMap[to_string(level)]["playerX"];
   offsetX = screenWidth/(2*levelWidth+2);
   offsetY = (screenHeight/2)-((levelHeight*(screenWidth/(levelWidth+1)))/2);
+  ImageResizeNN(&imageCornerTopLeft, screenWidth/(levelWidth+1), screenWidth/(levelWidth+1));
+  ImageResizeNN(&imageCornerTopRight, screenWidth/(levelWidth+1), screenWidth/(levelWidth+1));
+  ImageResizeNN(&imageCornerBottomLeft, screenWidth/(levelWidth+1), screenWidth/(levelWidth+1));
+  ImageResizeNN(&imageCornerBottomRight, screenWidth/(levelWidth+1), screenWidth/(levelWidth+1));
+  ImageResizeNN(&imageWallUp, screenWidth/(levelWidth+1), screenWidth/(levelWidth+1));
+  ImageResizeNN(&imageWallDown, screenWidth/(levelWidth+1), screenWidth/(levelWidth+1));
+  ImageResizeNN(&imageWallLeft, screenWidth/(levelWidth+1), screenWidth/(levelWidth+1));
+  ImageResizeNN(&imageWallRight, screenWidth/(levelWidth+1), screenWidth/(levelWidth+1));
+  ImageRotate(&imageCornerTopRight,90);
+  ImageRotate(&imageCornerBottomLeft,180);
+  ImageRotate(&imageCornerBottomRight,270);
+  ImageRotate(&imageWallDown,180);
+  ImageRotate(&imageWallLeft,270);
+  ImageRotate(&imageWallRight,90);
 }
 
 void draw() {
-  ClearBackground(BLACK);
-  // DrawText(TextFormat("PlayerX: %i",playerX), 10, 10, 20, WHITE);
+  Texture2D textureCornerTopLeft = LoadTextureFromImage(imageCornerTopLeft);
+  Texture2D textureCornerTopRight = LoadTextureFromImage(imageCornerTopRight);
+  Texture2D textureCornerBottomLeft = LoadTextureFromImage(imageCornerBottomLeft);
+  Texture2D textureCornerBottomRight = LoadTextureFromImage(imageCornerBottomRight);
+  Texture2D textureWallUp = LoadTextureFromImage(imageWallUp);
+  Texture2D textureWallDown = LoadTextureFromImage(imageWallDown);
+  Texture2D textureWallLeft = LoadTextureFromImage(imageWallLeft);
+  Texture2D textureWallRight = LoadTextureFromImage(imageWallRight);
+  ClearBackground(ColorFromHSV(302,.54,.52));
   for (size_t rowIndex = 0; rowIndex < levelMap[to_string(level)]["tilemap"].size(); ++rowIndex) {
     const auto& row = levelMap[to_string(level)]["tilemap"][rowIndex];
     for (size_t tileIndex = 0; tileIndex < row.size(); ++tileIndex) {
       const auto& tile = row[tileIndex];
-      if (tile.get<int>() != 0) {
-        DrawRectangle(offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), (screenWidth/(levelWidth+1))-1, (screenWidth/(levelWidth+1))-1, WHITE);
-      }
+      if (tile.get<int>() == 1) DrawTexture(textureCornerTopLeft, offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), WHITE);
+      if (tile.get<int>() == 2) DrawTexture(textureCornerTopRight, offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), WHITE);
+      if (tile.get<int>() == 3) DrawTexture(textureCornerBottomLeft, offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), WHITE);
+      if (tile.get<int>() == 4) DrawTexture(textureCornerBottomRight, offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), WHITE);
+      if (tile.get<int>() == 5) DrawTexture(textureWallUp, offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), WHITE);
+      if (tile.get<int>() == 6) DrawTexture(textureWallDown, offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), WHITE);
+      if (tile.get<int>() == 7) DrawTexture(textureWallLeft, offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), WHITE);
+      if (tile.get<int>() == 8) DrawTexture(textureWallRight, offsetX+tileIndex*(screenWidth/(levelWidth+1)), offsetY+rowIndex*(screenWidth/(levelWidth+1)), WHITE);
     }
   }
 }
+//272,80,48
+//302,54,52
